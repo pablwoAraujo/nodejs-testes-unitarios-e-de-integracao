2b717f27990db738ed53a706049f83ba3b4594c7
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-03-20 08:39:38 -0300

    tests: criando um mock para simular a chamada ao db

--- livraria/src/test/models/editora.test.js
+++ livraria/src/test/models/editora.test.js
@@ -1,4 +1,6 @@
-import { describe, expect, it } from '@jest/globals';
+import {
+  describe, expect, it, jest,
+} from '@jest/globals';
 import Editora from '../../models/editora';
 
 describe('Testando o modelo Editora', () => {
@@ -24,13 +26,37 @@ describe('Testando o modelo Editora', () => {
     });
   });
 
-  it('Deve salvar a editora no banco de dados usando a sintaxe moderna', async () => {
+  it.skip('Deve salvar a editora no banco de dados usando a sintaxe moderna', async () => {
     const editora = new Editora(objEditora);
 
     const dados = await editora.salvar();
     const dadosDoBanco = await Editora.pegarPeloId(dados.id);
 
     expect(dadosDoBanco).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
+
+  it('Deve fazer uma chamada simulada ao banco de dados', () => {
+    const editora = new Editora(objEditora);
+
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 1,
+      nome: 'CDC',
+      cidade: 'Campina Grande',
+      email: 'cdc@g.com',
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
+
+    const retorno = editora.salvar();
+
+    expect(retorno).toEqual(
       expect.objectContaining({
         id: expect.any(Number),
         ...objEditora,
